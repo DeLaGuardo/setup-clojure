21d73af8da35d213a919cf0d431cf64b2c6b42ac
Kirill Chernyshov <kchernys@cisco.com>
2022-11-22 11:58:27 +0100

    Bump version

--- dist/index.js
+++ dist/index.js
@@ -1139,7 +1139,7 @@ exports.isMacOS = isMacOS;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.VERSION = void 0;
-exports.VERSION = '10-0';
+exports.VERSION = '10-1';
 
 
 /***/ }),
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = '10-0'
+export const VERSION = '10-1'
