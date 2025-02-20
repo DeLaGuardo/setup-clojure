7fadaea9d3d290584627e45ae52290065536a687
Kirill Chernyshov <kchernys@cisco.com>
2025-02-20 16:40:45 +0100

    wip

--- src/setup-clojure.ts
+++ src/setup-clojure.ts
@@ -1,8 +1,12 @@
 import {pre, post, main} from './entrypoint'
+import * as core from '@actions/core'
 
 async function run(): Promise<void> {
+  core.debug('=== Run pre-setup clojure ===')
   await pre()
+  core.debug('=== Run setup clojure ===')
   await main()
+  core.debug('=== Run post-setup clojure ===')
   await post()
 }
 
